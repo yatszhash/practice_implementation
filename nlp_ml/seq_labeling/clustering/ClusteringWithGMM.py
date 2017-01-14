fix covariance, posterior prob and jointed prob of gmm and init with kmeans

nlp_ml/seq_labeling/clustering/ClusteringWithGMM.py
@@ -2,6 +2,8 @@ import logging
 import sys
 
 import numpy as np
+from numpy.linalg import pinv
+from sklearn.cluster import KMeans
 
 
 class ClusteringWithGMM:
@@ -35,6 +37,8 @@ class ClusteringWithGMM:
         self._previous_log_likelihood = -sys.maxsize
         self._log_liklihood_history = None
 
+        self._mixed_dist = None
+
         self._stop_threshold = 1e-4
 
     def fit(self, X, num_clusters=10, random_state=None):
@@ -65,9 +69,7 @@ class ClusteringWithGMM:
         return result
 
     def _predict_each_x(self, x):
-        vfunc = lambda c_idx: self._compute_posterior_prob(x, c_idx)
-        # sum doesn't need.
-        probs = np.apply_along_axis(vfunc, 0, np.arange(self._num_clusters))
+        probs = self._compute_posterior_probs_on_new_data(x)
 
         cluster_num = np.argmax(a=probs, axis=0)
 
@@ -83,7 +85,8 @@ class ClusteringWithGMM:
         self._update_all_posterior_probs()
         self._compute_new_params()
 
-        self._update_all_joint_distribution()
+        self._update_all_mixed_dist()
+        self._update_joint_distribution()
 
         log_liklihood = self._compute_log_liklihood()
         self._log_liklihood_history[self._num_iter] = log_liklihood
@@ -113,8 +116,7 @@ class ClusteringWithGMM:
 
         if np.isnan(self._current_cluster_avgs).any() or \
                 np.isnan(self._current_covariances).any() or \
-                np.isnan(self._current_cluster_probs).any() or \
-                (self._current_covariances == 0).any():
+                np.isnan(self._current_cluster_probs).any():
             logging.warning("some paramaters include Nan. "
                             "The learning was failed and terminated forcibly")
             isFinished = True
@@ -124,10 +126,36 @@ class ClusteringWithGMM:
 
     def _init_params(self):
         np.random.seed(self._random_state)
+        #
+        # self._init_avgs()
+        # self._init_covariances()
+        # self._init_cluster_probs()
 
-        self._init_avgs()
-        self._init_covariances()
-        self._init_cluster_probs()
+        self._init_with_kmeans()
+
+        self._update_all_mixed_dist()
+        self._update_joint_distribution()
+
+    def _init_with_kmeans(self):
+        estimator = KMeans(n_clusters=self._num_clusters)
+        estimator.fit(self._train_X)
+
+        init_Y = estimator.predict(self._train_X)
+
+        c_indice = np.arange(self._num_clusters)
+        c_avg = lambda c_idx: np.mean(self._train_X[init_Y == c_idx], axis=0)
+        self._current_cluster_avgs = np.array([c_avg(c_idx)
+                                               for c_idx in c_indice])
+
+        self._new_avgs = self._current_cluster_avgs
+
+        c_cov = lambda c_idx: np.cov(self._train_X[init_Y == c_idx].T)
+        self._current_covariances = np.array([c_cov(c_idx) for c_idx in c_indice])
+        self._new_covariances = self._current_covariances
+
+        self._current_cluster_probs = np.unique(init_Y, return_counts=True)[1] \
+                                      / self._dataset_size
+        self._new_cluster_probs = self._current_cluster_probs
 
     def _init_avgs(self):
         self._current_cluster_avgs = np.random.rand(self._num_clusters,
@@ -143,59 +171,134 @@ class ClusteringWithGMM:
 
     def _init_covariances(self):
         # self._current_covariances = self._compute_new_covariances()
-        self._current_covariances = np.random.rand(self._num_clusters) / 4
+        self._current_covariances = np.random.rand(self._num_clusters,
+                                                   self._dimension, self._dimension) / 4
         self._new_covariances = self._current_covariances
 
     def _compute_log_liklihood(self):
         return np.log(self._joint_distribution.sum(axis=1)) \
             .sum(axis=0)
 
+
     def _update_all_posterior_probs(self):
-        self._posterior_probs = np.zeros((self._dataset_size,
-                                          self._num_clusters))
-        x_indices = np.arange(self._dataset_size)
-        for i in x_indices:
-            self._posterior_probs[i, :] \
-                = self._compute_posterior_prob_on_each_x(i)
+        self._posterior_probs = np.array([
+                                             self._mixed_dist[x_idx, :] / self._joint_distribution[x_idx]
+                                             for x_idx in np.arange(self._dataset_size)
+                                             ])
 
-    def _compute_posterior_prob_on_each_x(self, x_idx):
-        vfunc = np.vectorize(lambda c_idx:
-                             self._compute_posterior_prob(self._train_X[x_idx, :], c_idx))
-        posterior_probs = np.apply_along_axis(vfunc, 1,
-                                              np.arange(self._num_clusters)
-                                              .reshape(1, self._num_clusters))
+    def _compute_posterior_probs_on_new_data(self, x):
 
-        summed = posterior_probs.sum()
+        return np.array([self._compute_mixture_distribution(x, c_idx)
+                         for c_idx in np.arange(self._num_clusters)])
 
-        return posterior_probs / summed
+    '''
+    # def _update_all_posterior_probs(self):
+    #     self._posterior_probs = np.zeros((self._dataset_size,
+    #                                       self._num_clusters))
+    #     x_indices = np.arange(self._dataset_size)
+    #     for i in x_indices:
+    #         self._posterior_probs[i, :] \
+    #             = self._compute_posterior_prob_on_each_x(i)
 
-    def _compute_posterior_prob(self, x, c_idx):
-        return self._current_cluster_probs[0, c_idx] \
-               * np.exp(
-            - np.linalg.norm(x - self._current_cluster_avgs[c_idx, :]) ** 2 \
-            / (2 * self._current_covariances[c_idx]))
+    # def _compute_posterior_prob_on_each_x(self, x_idx):
+    #     vfunc = np.vectorize(lambda c_idx:
+    #                          self._compute_posterior_prob(self._train_X[x_idx, :], c_idx))
+    #     posterior_probs = np.apply_along_axis(vfunc, 1,
+    #                                           np.arange(self._num_clusters)
+    #                                           .reshape(1, self._num_clusters))
+    #
+    #     summed = posterior_probs.sum()
+    #
+    #     return posterior_probs / summed
 
-    def _update_all_joint_distribution(self):
-        self._joint_distribution = np.zeros((self._dataset_size,
+    # def _compute_posterior_prob(self, x, c_idx):
+    #     diff = (x - self._current_cluster_avgs[c_idx, :]).reshape(self._dimension, 1)
+    #     cov = self._current_covariances[c_idx, :, :]
+    #     co_inv = pinv(cov)
+    #     first = np.dot(diff.T, co_inv)
+    #     second = np.dot(first, diff)
+    #     in_exp = np.exp(- 1 / 2 * second)
+    #
+    #     det = (2 * np.pi) ** (1 / self._dimension) * np.linalg.det(cov) ** (1 / 2)
+    #
+    #     result = self._new_cluster_probs[0, c_idx] / det * in_exp
+    #
+    #     return self._current_cluster_probs[0, c_idx] \
+    #            * np.exp(
+    #         - np.linalg.norm(x - self._current_cluster_avgs[c_idx, :]) ** 2 \
+    #         / (2 * self._current_covariances[c_idx]))
+
+    # def _update_all_joint_distribution(self):
+    #     self._joint_distribution = np.zeros((self._dataset_size,
+    #                                          self._num_clusters))
+    #     for x_idx in np.arange(self._dataset_size):
+    #         self._joint_distribution[x_idx, :] \
+    #             = self._compute_joint_distribution_on_each_x(x_idx)
+    #
+    # def _compute_joint_distribution_on_each_x(self, x_idx):
+    #     vfunc = lambda c_idx: self._compute_joint_distribution(
+    #         self._train_X[x_idx, :], c_idx )
+    #
+    #     result =  np.array([vfunc(c_idx) for c_idx in np.arange(self._num_clusters)])
+    #
+    #     return result.ravel().ravel()
+    #
+    #
+    # def _compute_joint_distribution(self, x, c_idx):
+    #     diff = (x - self._new_avgs[c_idx, :]).reshape(self._dimension, 1)
+    #     cov = self._new_covariances[c_idx, :, :]
+    #     co_inv = pinv(cov)
+    #     first = np.dot(diff.T, co_inv)
+    #     second = np.dot(first, diff)
+    #     in_exp = np.exp(- 1 / 2 * second)
+    #
+    #     det = (2 * np.pi) ** (1 / self._dimension) * np.linalg.det(cov) ** (1 /2)
+    #
+    #     result = self._new_cluster_probs[0, c_idx]  / det * in_exp
+    #
+    #     return result
+    '''
+
+    def _update_joint_distribution(self):
+        each_p_x = lambda x_idx: \
+            np.dot(self._new_cluster_probs.reshape(1, self._num_clusters),
+                   self._mixed_dist[x_idx, :].T)
+        p_x_0 = np.dot(self._new_cluster_probs.reshape(1, self._num_clusters),
+                       self._mixed_dist[0, :].T)
+
+        all_p_x = np.apply_along_axis(each_p_x, 1,
+                                      np.arange(self._dataset_size).reshape(self._dataset_size, 1))
+
+        self._joint_distribution = all_p_x
+
+    def _update_all_mixed_dist(self):
+        self._mixed_dist = np.zeros((self._dataset_size,
                                              self._num_clusters))
         for x_idx in np.arange(self._dataset_size):
-            self._joint_distribution[x_idx, :] \
-                = self._compute_joint_distribution_on_each_x(x_idx)
+            self._mixed_dist[x_idx, :] \
+                = self._compute_mixture_distribution_on_each_x(x_idx)
 
-    def _compute_joint_distribution_on_each_x(self, x_idx):
-        vfunc = np.vectorize(lambda c_idx: self._compute_joint_distribution(
-            self._train_X[x_idx, :], c_idx
-        ))
+    def _compute_mixture_distribution_on_each_x(self, x_idx):
+        vfunc = lambda c_idx: self._compute_mixture_distribution(
+            self._train_X[x_idx, :], c_idx)
 
-        return np.apply_along_axis(vfunc, 1,
-                                   np.arange(self._num_clusters)
-                                   .reshape(1, self._num_clusters))
+        result = np.array([vfunc(c_idx) for c_idx in np.arange(self._num_clusters)])
 
-    def _compute_joint_distribution(self, x, c_idx):
-        return self._new_cluster_probs[0, c_idx] \
-               / (np.sqrt((2 * np.pi * self._new_covariances[c_idx]) ** self._dimension)) \
-               * np.exp(- np.linalg.norm(x - self._new_avgs[c_idx, :]) ** 2 \
-                        / (2 * self._new_covariances[c_idx]))
+        return result.ravel().ravel()
+
+    def _compute_mixture_distribution(self, x, c_idx):
+        diff = (x - self._new_avgs[c_idx, :]).reshape(self._dimension, 1)
+        cov = self._new_covariances[c_idx, :, :]
+        co_inv = pinv(cov)
+        first = np.dot(diff.T, co_inv)
+        second = np.dot(first, diff)
+        in_exp = np.exp(- 1 / 2 * second)
+
+        det = (2 * np.pi) ** (1 / self._dimension) * np.linalg.det(cov) ** (1 / 2)
+
+        result = 1 / det * in_exp
+
+        return result
 
     def _compute_new_params(self):
         self._new_avgs = self._compute_new_avgs()
@@ -224,22 +327,44 @@ class ClusteringWithGMM:
         return self._posterior_probs[x_idx, c_idx] \
                * self._train_X[x_idx, :]
 
+    '''
+    # def _compute_new_covariances(self):
+    #     return np.array([self._compute_new_cluster_covariance(c_idx)
+    #                      for c_idx in np.arange(self._num_clusters)])
+    #
+    # def _compute_new_cluster_covariance(self, c_idx):
+    #     diff_sum_func = lambda x_idx: \
+    #         self._posterior_probs[x_idx, c_idx] * \
+    #         np.dot(self._train_X[x_idx, :] - self._new_avgs[c_idx, :],
+    #                (self._train_X[x_idx, :] - self._new_avgs[c_idx, :]).T)
+    #
+    #     marginal = self._posterior_probs.sum(axis=0)[c_idx]
+    #
+    #     diff_sum = np.array([diff_sum_func(x_idx) / marginal
+    #                          for x_idx in np.arange(self._dataset_size)]).sum(axis=0)
+    #
+    #     return diff_sum
+    '''
+
     def _compute_new_covariances(self):
         return np.array([self._compute_new_cluster_covariance(c_idx)
                          for c_idx in np.arange(self._num_clusters)])
 
     def _compute_new_cluster_covariance(self, c_idx):
+        diff = lambda x_idx: (self._train_X[x_idx, :] - self._new_avgs[c_idx, :]) \
+            .reshape(self._dimension, 1)
+
         diff_sum_func = lambda x_idx: \
             self._posterior_probs[x_idx, c_idx] * \
-            np.dot(self._train_X[x_idx, :] - self._new_avgs[c_idx, :],
-                   (self._train_X[x_idx, :] - self._new_avgs[c_idx, :]).T)
+            np.dot(diff(x_idx), diff(x_idx).T)
 
         marginal = self._posterior_probs.sum(axis=0)[c_idx]
 
-        diff_sum = np.array([diff_sum_func(x_idx) / marginal
+        diff_sum = np.array([diff_sum_func(x_idx)
                              for x_idx in np.arange(self._dataset_size)]).sum(axis=0)
 
-        return diff_sum
+        return diff_sum / marginal
+
 
     def _compute_new_cluster_prob(self):
         return np.apply_along_axis(

nlp_ml/test/clustreing/test_clusteringWithNMD.py
@@ -136,25 +136,32 @@ class TestClusteringWithNMD(TestCase):
 
     def test__update_all_joint_distribution(self):
         sut = ClusteringWithGMM()
-        sut._train_X = np.array([[1, 4, 7, 10],
-                                 [2, 5, 8, 11],
-                                 [3, 6, 9, 12]])
+        sut._train_X = np.array([[1, 4, 7],
+                                 [2, 5, 8],
+                                 [3, 6, 9],
+                                 [4, 7, 10]])
 
-        sut._dimension = 4
-        sut._dataset_size = 3
+        sut._dimension = 3
+        sut._dataset_size = 4
         sut._num_clusters = 2
-        sut._new_avgs = np.array([[2, 5, 8, 11],
-                                  [1, 4, 7, 10]])
+        sut._new_avgs = np.array([[2, 5, 8],
+                                  [1, 4, 7]])
 
         sut._new_cluster_probs = np.array([[1 / 3, 2 / 3]])
-        sut._new_covariances = np.array([4, 4])
+        sut._new_covariances = np.array([[[-1, -2, -3],
+                                          [-6, -4, -5],
+                                          [3, 2, 1]],
+
+                                         [[2, 0, 3],
+                                          [3, 0, 5],
+                                          [-2, -2, -1]]])
 
         expected = np.array([
-            [1 / (192 * np.pi ** 2) * np.exp(- 1 / 2), 1 / 96 / np.pi ** 2],
-            [1 / (192 * np.pi ** 2), 1 / 96 * np.exp(-1 / 2) / np.pi ** 2],
-            [1 / (192 * np.pi ** 2) * np.exp(- 1 / 2),
-             1 / 96 * np.exp(- 2) / np.pi ** 2]
-        ])
+            [(1 / 12) ** (1 / 2) * np.exp(- 13 / 12), 1 / 2 ** (1 / 2)],
+            [(1 / 12) ** (1 / 2), 1 / (2) ** (1 / 2) * np.exp(1 / 2)],
+            [(1 / 12) ** (1 / 2) * np.exp(- 13 / 12), 1 / 2 ** (1 / 2) * np.exp(2)],
+            [(1 / 12) ** (1 / 2) * np.exp(- 13 / 3), 1 / 2 ** (1 / 2) * np.exp(3)]
+        ]) / (2 * np.pi) ** (1 / 3)
 
         sut._update_all_joint_distribution()
 
@@ -182,6 +189,7 @@ class TestClusteringWithNMD(TestCase):
 
     def test_compute_new_covariances(self):
         sut = ClusteringWithGMM()
+
         sut._train_X = np.array([[1, 4, 7, 10],
                                  [2, 5, 8, 11],
                                  [3, 6, 9, 12]])
@@ -197,10 +205,18 @@ class TestClusteringWithNMD(TestCase):
         ])
 
         sut._new_avgs = np.array([[2, 5, 8, 11],
-                                  [1, 4, 7, 10]])
+                                  [1, 3, 5, 7]])
 
-        expected = np.array([72 / 23, 252 / 37])
+        expected = np.array([[[18 / 23, 18 / 23, 18 / 23, 18 / 23],
+                              [18 / 23, 18 / 23, 18 / 23, 18 / 23],
+                              [18 / 23, 18 / 23, 18 / 23, 18 / 23],
+                              [18 / 23, 18 / 23, 18 / 23, 18 / 23]],
 
-        actual = sut._compute_new_covariances()
+                             [[63 / 37, 102 / 37, 141 / 37, 180 / 37],
+                              [102 / 37, 178 / 37, 254 / 37, 330 / 37],
+                              [141 / 37, 254 / 37, 367 / 37, 480 / 37],
+                              [180 / 37, 330 / 37, 480 / 37, 630 / 37]]])
+
+        actual = sut._fixed_compute_new_covariances()
 
         np.testing.assert_array_almost_equal(actual, expected)